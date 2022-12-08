58f6a5087635c9a6ab8b370b5a958896784e9a45
Ivan Hart <ihart@ivanhart.dev>
2022-12-08 17:09:02 -0600

    changed project file structure

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-import encode from './encode.js'
-import decode from './decode.js'
+function encode(message, alp) {
+
+    message = message.toLowerCase()
+
+    let keyArray = []
+
+    let codeArray = []
+
+    for (let i = 0; i < message.length; i++) {
+
+        let key = Math.floor(Math.random() * 10) + 1
+
+        keyArray.push(key)
+
+        for (let j = 0; j < alp.length; j++) {
+
+            if (message[i] === alp[j]) {
+
+                codeArray.push((j + 1) * key)
+
+            }
+        }
+    }
+
+    message = ''
+
+    for (let i = 0; i < codeArray.length; i++) {
+
+        message += codeArray[i] + " " + keyArray[i] + " "
+
+    }
+
+    return message
+}
+
+function decode(message, alp) {
+
+    message = message.split(" ")
+
+    res = ''
+
+    for (let i = 0; i < message.length - 1; i+=2) {
+
+        let index = parseInt(message[i] / message[i+1])
+
+        res += alp[index - 1]
+    }
+
+    return res
+}
 
 const alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', ',', '.', '?', ' ']
 
